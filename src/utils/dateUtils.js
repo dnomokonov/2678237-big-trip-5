0fef5f32c8c6c012d9ba62600e8722f47aa6876f
dnomokonov <zmnomokon715@gmail.com>
2026-02-12 23:20:24 +0700

    Add datepicker

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -5,6 +5,7 @@ const VISIBLE_TIME = 'HH:mm';
 const VISIBLE_SHORT_DATE = 'MMM D';
 const MACHINE_DATE = 'YYYY-MM-DD';
 const MACHINE_DATETIME = 'YYYY-MM-DDTHH:mm';
+const FORM_DATEPICKER = 'd/m/y H:i';
 
 function formatForInput(date) {
   return dayjs(date).format(FORM_INPUT_FULL);
@@ -57,5 +58,6 @@ export {
   formatVisibleShortDate,
   formatMachineDate,
   formatMachineDateTime,
-  formatDuration
+  formatDuration,
+  FORM_DATEPICKER
 };
